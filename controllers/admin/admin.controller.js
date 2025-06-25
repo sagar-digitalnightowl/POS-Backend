ba0f61da2c46862a0445e839cf0cfc773989a4b5
sagar-digitalnightowl <sagar@digitalnightowl.in>
2025-06-25 12:14:47 +0530

    fix cookie remove issue

--- controllers/admin/admin.controller.js
+++ controllers/admin/admin.controller.js
@@ -209,9 +209,9 @@ routes.logout = async (req, res) => {
     res.clearCookie("token", {
       path: "/",
       sameSite: "Lax",
-      secure: false,
+      secure: true,
     });
-    
+
     res.status(200).json({ message: "Logged out" });
   } catch (error) {
     console.log("error=", error);
